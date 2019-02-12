01d0c40dbab74fb8ad46cbbb0a1a48cbdd96ae53
mattgitsit <39727866+mattgitsit@users.noreply.github.com>
2019-02-12 02:15:19 -0800

    added user registration

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/apis/users');
 const profile = require('./routes/apis/profile');
 const posts = require('./routes/apis/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
